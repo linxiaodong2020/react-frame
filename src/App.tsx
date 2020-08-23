2d8d972b36d8173a5b95fd06233235c4d5eabe2a
halle <linxiaodong19962@163.com>
2020-08-23 22:15:26 +0800

    feat:主体框架

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from 'react';
+import { HashRouter as Router, Link, Route } from 'react-router-dom'
+import Home from './pages/home';
+export interface IAppProps {
 }
 
-export default App;
+export default class App extends React.Component<IAppProps> {
+  public render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <hr />
+          <Route path="/" exact component={Home}></Route>
+        </div>
+      </Router>
+    );
+  }
+}
---
+++ src/pages/frame/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IFrameProps {
+}
+
+export default class Frame extends React.Component<IFrameProps> {
+    public render() {
+        return (
+            <div>
+                frame
+            </div>
+        );
+    }
+}
